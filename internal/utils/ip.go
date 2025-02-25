e4781135d475062e6f3775832b68328ee8876dc8
sebomancien <sebomancien@hotmail.com>
2025-02-24 21:13:13 -0500

    Added curl agent header to avoid receving the whole html response

--- internal/utils/ip.go
+++ internal/utils/ip.go
@@ -1,13 +1,24 @@
 package utils
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"net/http"
 )
 
 // Get the current public IP address
 func GetPublicIp() (string, error) {
-	resp, err := http.Get("https://ifconfig.me")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://ifconfig.me", nil)
+	if err != nil {
+		return "", err
+	}
+
+	// Mimic curl to avoid the website returning the whole HTML page
+	req.Header.Set("User-Agent", "curl/7.64.1")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
@@ -18,5 +29,11 @@ func GetPublicIp() (string, error) {
 		return "", err
 	}
 
-	return string(body), nil
+	// Validate that the IP address is in the correct format
+	ip := net.ParseIP(string(body))
+	if ip == nil {
+		return "", fmt.Errorf("failed to parse ip address: %s", body)
+	}
+
+	return ip.String(), nil
 }
